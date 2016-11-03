f3376e51999290c455d3ca5e1bb2284ad348c9fc
mathuramg <mmg9291@gmail.com>
2016-11-02 22:13:56 -0400

    add multiple objects

--- interceptors/interceptPan.js
+++ interceptors/interceptPan.js
@@ -24,6 +24,12 @@ var oscillatorNode = audioCtx.createOscillator();
 var gainNode = audioCtx.createGain();
 var panNode = audioCtx.createStereoPanner();
 
+//initialise parameters
+var objectCount = 0;
+var currFrame = 2;
+var prevFrame = 2;
+var objects = [];
+
 funcNames = funcNames.filter(function(x) {
   var className = x["class"];
   return (x["name"] && x["params"] && (className==='p5'));
@@ -56,33 +62,59 @@ funcNames.forEach(function(x){
 
     // Pull out only the shapes in draw()
     else if(frameCount > 1 && (frameCount%1 == 0) && x.module=='Shape') {
+
+      if(frameCount != currFrame) {
+        currFrame ++;
+        objectCount = 0;
+      }
+      objectCount ++;
+      console.log("object count"  + objectCount);
+
+      if(!objects[objectCount-1]){
+          objects[objectCount-1] = new Object({
+          xPosCurr:0,
+          xPosDiff:0,
+          xPosPrev:0,
+          yPosCurr:0,
+          yPosDiff:0,
+          yPosPrev:0
+        });
+      }
       //pull out only the x coord values and compare with prev value
       for(var i =0; i < x.params.length; i++) {
         if(x.params[i].description.indexOf('x-coordinate')>-1){
-          // console.log('in here');
-          xPosCurr = arguments[i];
-          xPosDiff = xPosCurr - xPosPrev;
-          xPosPrev = xPosCurr;
+
+          objects[objectCount-1].xPosCurr = arguments[i];
+          objects[objectCount-1].xPosDiff = objects[objectCount-1].xPosCurr - objects[objectCount-1].xPosPrev;
+          objects[objectCount-1].xPosPrev = objects[objectCount-1].xPosCurr;
+          console.log(objects[objectCount-1].xPosPrev);
         }
         if(x.params[i].description.indexOf('y-coordinate')>-1){
-          yPosCurr = arguments[i];
-          yPosDiff = yPosCurr - yPosPrev;
-          yPosPrev = yPosCurr;
+          objects[objectCount-1].yPosCurr = arguments[i];
+          objects[objectCount-1].yPosDiff = objects[objectCount-1].yPosCurr - objects[objectCount-1].yPosPrev;
+          objects[objectCount-1].yPosPrev = objects[objectCount-1].yPosCurr;
         }
-        if(abs(xPosDiff>0)||abs(yPosDiff>0))
+        if(abs(objects[objectCount-1].xPosDiff>0)||abs(objects[objectCount-1].yPosDiff>0))
         {
-          currNote = (1-yPosCurr/height)*(12); // mapping hieghts to notes from 1-100
+          currNote = (1-objects[objectCount-1].yPosCurr/height)*(12); // mapping hieghts to notes from 1-100
           //fn = f0 * (a)n
           currLogFreq = 440 * Math.pow(Math.pow(2,(1/12)),currNote);
           currVol = 0.4;
           x_coord = frameCount%10 - 5;
-          currVol = 1*Math.exp(-(x_coord*x_coord));
-          currPan = (xPosCurr/width)*2 - 1;
+          console.log(x_coord + ' - ' +frameCount);
+          currVol = 2*objectCount*Math.exp(-((x_coord+2*objectCount)*(x_coord+2*objectCount)));
+          console.log(currLogFreq + ' -- ' + currPan + ' -- ' + currVol );
+          currPan = (objects[objectCount-1].xPosCurr/width)*2 - 1;
           oscillatorNode.frequency.value = currLogFreq;
           gainNode.gain.value = currVol;
           panNode.pan.value = currPan;
         }
+        else {
+          console.log('in here');
+          gainNode.gain.value = 0;
+        }
       }
+
     }
     return originalFunc.apply(this,arguments);
   }
